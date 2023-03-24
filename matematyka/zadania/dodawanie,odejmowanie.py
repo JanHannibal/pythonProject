8b44deb13cdce2c0f653fe20754428851310f023
JanHannibal <jan.budek2013@gmail.com>
2023-03-24 20:18:03 +0100

    dodane menu

--- matematyka/zadania/dodawanie,odejmowanie.py
+++ matematyka/zadania/dodawanie,odejmowanie.py
@@ -1,18 +1,18 @@
 import random
-za = True
-while za:
+
+print('| PROGRAM DO NAUKI DODAWANIA I ODEJMOWANIA |')
+
+print('AUTOR: JAN HANNIBAL')
+
+powtarzanie_petli = 1000
+
+print(' ')
+maksymalne_cyfry_w_dodawaniu = input('jaką maksymalną liczbę lub cyfre chcesz dodać  ')
+
+while powtarzanie_petli > 0:
     try:
-        hcenie = input(' 1 = następne działanie,2 = wyjdź  ')
-        if hcenie == '1':
-            print('ok')
 
-        elif hcenie == '2':
-            print('dowidzenia')
-            za = False
-            break
-
-        else:
-            exit('błąd zrobiłeś')
+        powtarzanie_petli = powtarzanie_petli - 1
 
         znaki_dzialan_matematycznych = ['+', '-']
 
@@ -20,8 +20,8 @@ while za:
 
         wylosowany_znak_dzialan_matematycznych = znaki_dzialan_matematycznych[wylosowany_znak]
 
-        liczba1 = random.randint(0, 10)
-        liczba2 = random.randint(0, 10)
+        liczba1 = random.randint(0, int(maksymalne_cyfry_w_dodawaniu))
+        liczba2 = random.randint(0, int(maksymalne_cyfry_w_dodawaniu))
 
         odpowiedz = int(input(str(liczba1) + ' ' + wylosowany_znak_dzialan_matematycznych + ' ' + str(liczba2) + ' = '))
 
@@ -32,9 +32,30 @@ while za:
             poprawna_odpowiedz = liczba1 - liczba2
 
         if odpowiedz == poprawna_odpowiedz:
+            print(' ')
             print('dobrze')
+            print(' ')
+        else:
+            print(' ')
+            print('źle')
+            print(' ')
+
+        print('-----  MENU  -----')
+        print('1 = następne działanie')
+        print('2 = wyjdź')
+        wybor_menu = input('twój wybór to: ')
+        print("--------------------")
+
+        if wybor_menu == '2':
+            print('do widzenia')
+            powtarzanie_petli = 0
+
+        elif wybor_menu == '1':
+            continue
 
         else:
-            print('źle')
+            exit('błąd zrobiłeś')
+
     except:
         print('miałeś podać liczbę')
+    print( )
