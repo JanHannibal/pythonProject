4d5dba4d011cce088e3008d0b68ec9101e02f2b5
JanHannibal <jan.budek2013@gmail.com>
2024-10-29 10:27:20 +0100

    zmiana 0-100 na 0-50

--- gry/game of numbers.py
+++ gry/game of numbers.py
@@ -2,6 +2,7 @@ import random
 i = 0
 d = 0
 y = 0
+lev2 = 0
 licz = 0
 print('English , Polski')
 d = input()
@@ -10,14 +11,14 @@ if d == 'English':
 elif d == 'Polski':
     i = 3
 
-random_number = random.randrange(0, 100)
+random_number = random.randrange(0, 50)
 
 if d == 'English':
     print('Author czwartoklasista z laptopem https://www.youtube.com/results?search_query=czwartoklasista+z+laptopem')
 elif d == 'Polski':
     print('Autor Czwartoklasista z laptopem https://www.youtube.com/results?search_query=czwartoklasista+z+laptopem')
-print('wpisz jaka liczba lub cyfra się wylosowała')
-print('enter the number or digit that was drawn')
+print('wpisz jaka liczba lub cyfra się wylosowała 0-50')
+print('enter the number or digit that was drawn 0-50')
 while i == 3:
 
     y = y + 1
@@ -56,6 +57,242 @@ if d == 'English':
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    lev2 == 1
+    print('you made it to the next level')
+elif y <= 3 and d == 'Polski':
+    lev2 == 1
+    print("dostałeś się do następnego levelu")
+else:
+    exit()
+if lev2 == 1:
+    random_number = random.randrange(0, 100)
+    print('wpisz jaka liczba lub cyfra się wylosowała 0-100')
+    print('enter the number or digit that was drawn 0-100')
+    while i == 3:
+
+        y = y + 1
+        p = int(input())
+        if random_number == p:
+            i = i + 1
+
+        else:
+            print('źle')
+            if p < random_number:
+                print(' to jest liczba większa niż', p)
+            else:
+                print('to jest liczba mniejsza niż', p)
+    if d == 'Polski':
+        print('zgadłeś za', y, 'razem')
+
+    while licz == 1:
+
+        y = y + 1
+        t = int(input())
+        if random_number == t:
+            licz = licz + 1
+
+        else:
+            print('Badly')
+            if t < random_number:
+                print(' this is a number greater than', t)
+            else:
+                print('this is a number less than', t)
+    if y <= 2 and d == 'English':
+        print('you win secret level')
+    elif y <= 2 and d == 'Polski':
+        print('wygrałeś sekretny level')
+    else:
+        print('przegrałeś sekretny level')
+        print('you lost in secret level')
 
 
 
